06b6e1566d9a9991d8a10672c2fd5fd578ccef0b
alun <alunpw>
2022-10-20 23:11:54 +0700

    add extensions

---
+++ Suitmedia Test/Scenes/Welcome/WelcomeVC.swift
@@ -0,0 +1,29 @@
+//
+//  WelcomeVC.swift
+//  Suitmedia Test
+//
+//  Created by Alun Paranggi on 20/10/22.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
