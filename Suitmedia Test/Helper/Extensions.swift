0ad78145230ac45bc17f359b284a24a8ad1aa623
alun <alunpw>
2022-10-20 22:36:19 +0700

    add extension and implement isPalindrome function

--- Suitmedia Test/Helper/Extensions.swift
+++ Suitmedia Test/Helper/Extensions.swift
@@ -5,4 +5,20 @@
 //  Created by Alun Paranggi on 20/10/22.
 //
 
-import Foundation
+import UIKit
+
+extension String {
+    func isPalindrome() -> Bool {
+        if count < 2 { return false }
+        let str = lowercased().replacingOccurrences(of: " ", with: "")
+        let strArray = Array(str)
+        var i = 0
+        var j = strArray.count-1
+        while i <= j {
+            if strArray[i] != strArray[j] { return false }
+            i+=1
+            j-=1
+        }
+        return true
+    }
+}
