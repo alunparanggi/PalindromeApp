628e1ea215de5cca713001fa506277d560f68b2c
alun <alunpw>
2022-10-20 21:47:37 +0700

    init project

---
+++ Suitmedia Test/Scenes/Main/MainVC.swift
@@ -0,0 +1,18 @@
+//
+//  MainVC.swift
+//  Suitmedia Test
+//
+//  Created by Alun Paranggi on 20/10/22.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ Suitmedia Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Suitmedia Test
+//
+//  Created by Alun Paranggi on 20/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
